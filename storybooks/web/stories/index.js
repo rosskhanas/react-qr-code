b7b5673b8350e75400be2b5df6ef565f53c3dba3
Rostyslav Khanas <rtkhanas@gmail.com>
2017-02-22 08:51:01 +0900

    Update deps

--- storybooks/web/stories/index.js
+++ storybooks/web/stories/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
 import { storiesOf } from '@kadira/storybook';
+import React from 'react';
 import QRCode from 'react-qr-code';
 
 storiesOf('components', module)
-  .add('hey', () => (
-    <QRCode value="hey" />
+  .add('hello-world', () => (
+    <QRCode value="hello-world" />
+  ))
+  .add('my repository url', () => (
+    <QRCode value="https://github.com/rtkhanas/react-qr-code" />
+  ))
+  .add('my storybook url', () => (
+    <QRCode value="https://rtkhanas.github.io/react-qr-code/" />
   ));
