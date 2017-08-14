0e09603bda21c7321b07fde6a5482ad2c9356626
Rostyslav Khanas <rtkhanas@gmail.com>
2017-08-14 12:50:10 +0300

    New RN demo with expo

---
+++ storybooks/native/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Dimensions, StatusBar, TextInput, View } from 'react-native';
+
+import QRCode from './lib';
+
+const { height, width } = Dimensions.get('window');
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Hello, World!' };
+  }
+
+  render() {
+    return (
+      <View>
+        <StatusBar hidden />
+        <QRCode size={Math.min(height, width)} value={this.state.text} />
+        <TextInput
+          style={{ height: 50, borderColor: '#ccc', borderWidth: 1, margin: 10, padding: 5 }}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+        />
+      </View>
+    );
+  }
+}
--- storybooks/native/storybook/stories/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@kadira/react-native-storybook';
-import QRCode from '../../lib';
-
-storiesOf('components', module)
-  .add('hey', () => (
-    <QRCode value="hey" />
-  ));
