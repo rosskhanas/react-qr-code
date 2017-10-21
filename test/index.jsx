d995454de4e08dec76d6d6aebe79373c20d4efe6
Rostyslav Khanas <rtkhanas@gmail.com>
2017-10-21 14:44:14 +0300

    Merge Greenkeeper

--- test/index.jsx
+++ test/index.jsx
@@ -1,7 +1,7 @@
 import 'jsdom-global/register';
 import React from 'react';
 import { expect } from 'chai';
-import Enzyme, { mount, shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import QRCode from '../src';
 
@@ -11,12 +11,5 @@ describe('<QRCode /> shallow rendering', () => {
   it('has 441 children', () => {
     const wrapper = shallow(<QRCode value="hey" />);
     expect(wrapper.children()).to.have.length(441);
-  });
-});
-
-describe('<QRCode /> full DOM rendering', () => {
-  it('has 1 children', () => {
-    const wrapper = mount(<QRCode value="hey" />);
-    expect(wrapper.children()).to.have.length(1);
   });
 });
