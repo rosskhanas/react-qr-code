4912a179198d700e4bfa54acde62f55ec02555c5
Rostyslav Khanas <rtkhanas@gmail.com>
2020-05-03 13:39:38 +0300

    Updates

---
+++ precache-manifest.2cae0560843c67c69e5f03552841f2f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b620b241103f76a740957b0427bd0c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97eafa3c2a5aaa11cbc",
+    "url": "/static/js/2.61e5891c.chunk.js"
+  },
+  {
+    "revision": "ccf491244da6a3591a7f18b2d9dc8874",
+    "url": "/static/js/2.61e5891c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f68d28b3f75882d295d2",
+    "url": "/static/js/main.a2ddf492.chunk.js"
+  },
+  {
+    "revision": "af34574360f2e8c45495",
+    "url": "/static/js/runtime-main.916fb653.js"
+  }
+]);
