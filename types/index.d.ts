c2dc6cc1119c5b3cf44163076d63c8236e71026f
Jiang Zhu <jzhu1224@gmail.com>
2022-07-13 19:24:46 +1200

    Fixed QRCodeProps type doesn't have viewBox

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,7 @@
 declare module "react-qr-code" {
   import * as React from "react";
 
-  export interface QRCodeProps extends React.HTMLProps<SVGElement> {
+  export interface QRCodeProps extends React.SVGProps<SVGElement> {
     value: string;
     size?: number; // defaults to 128
     bgColor?: string; // defaults to '#FFFFFF'
