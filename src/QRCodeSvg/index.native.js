3686d5858865c5c3adb2fd376b87c71b649c975d
Marcus Egger <1050117+marcusegger@users.noreply.github.com>
2024-05-06 16:03:36 +0200

    Update index.native.js
    added xmlns and xmlns:xlink attributes for possibility to copy svg code and download it properly

--- src/QRCodeSvg/index.native.js
+++ src/QRCodeSvg/index.native.js
@@ -21,6 +21,7 @@ const QRCodeSvg = forwardRef(({ bgColor, bgD, fgD, fgColor, size, viewBoxSize, .
     style={{ height: size, width: size }}
     viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`}
     width={size}
+    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  
   >
     <Path d={bgD} fill={bgColor} />
     <Path d={fgD} fill={fgColor} />
