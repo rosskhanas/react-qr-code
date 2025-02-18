1478cbbed68fb48912e67a35f566143947a6c26c
branko-slavkov <155554076+branko-slavkov@users.noreply.github.com>
2025-02-18 19:39:17 +0100

    added title to missing props

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,9 +4,10 @@ declare module "react-qr-code" {
   export interface QRCodeProps extends React.SVGProps<SVGSVGElement> {
     value: string;
     size?: number; // defaults to 128
-    bgColor?: React.CSSProperties["backgroundColor"]; // defaults to '#FFFFFF'
-    fgColor?: React.CSSProperties["color"]; // defaults to '#000000'
-    level?: "L" | "M" | "H" | "Q"; // defaults to 'L'
+    bgColor?: React.CSSProperties["backgroundColor"]; // defaults to "#FFFFFF"
+    fgColor?: React.CSSProperties["color"]; // defaults to "#000000"
+    level?: "L" | "M" | "H" | "Q"; // defaults to "L"
+    title: string;
   }
 
   class QRCode extends React.Component<QRCodeProps, any> {
